efb09b8bd639f48d6a4eb9f2de98e3dfd96ef06e
xstiago <xs.tiago.91@gmail.com>
2017-08-15 11:16:05 -0300

    Início do desenvolvimento

---
+++ ChurrasTrinca/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChurrasTrinca.MvcApplication" Language="C#" %>
---
+++ ChurrasTrinca/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChurrasTrinca.Startup))]
+namespace ChurrasTrinca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
